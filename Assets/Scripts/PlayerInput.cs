7ae29fb3160f33b96fa8c2b26a5be61aaf969758
AslakDB <Askelakken@gmail.com>
2021-10-27 22:41:58 +0200

    la til et enkelt kamera auto scroller, med et input script med movement innebygd i input scriptet

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerInput : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+   public Vector2 moveVector;
+   public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector.x = (Keyboard.current.aKey.isPressed ? -0.25f : 0f) + (Keyboard.current.dKey.isPressed ? 1.5f : 0f);
+        moveVector.y = (Keyboard.current.sKey.isPressed ? 0f : 1f) + (Keyboard.current.wKey.isPressed ? 0f : -1f);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVector * moveSpeed * Time.fixedDeltaTime);
+    }
+}
